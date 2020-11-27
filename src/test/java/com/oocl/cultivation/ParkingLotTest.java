cd502b7ded26793c2caf127c5fc25023fc72acbb
Alex Liu <liual@oocl.com>
2020-11-27 15:41:39 +0800

    [ParkingBoy][Alex Liu] test: added test case return parking tickets when park multiple cars

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -16,5 +17,22 @@ public class ParkingLotTest {
 
         // then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_be_parked_when_park_multiple_cars_given_multiple_cars_and_parking_lot_with_available_capacity() {
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+
+        //when
+        final Ticket ticket1 = parkingLot.park(car1);
+        final Ticket ticket2 = parkingLot.park(car2);
+
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+        assertNotEquals(ticket1, ticket2);
     }
 }
