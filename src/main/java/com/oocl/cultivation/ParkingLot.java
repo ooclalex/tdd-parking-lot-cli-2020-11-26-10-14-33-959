8ee889b08cdc11e20cc8002e216cd7398d5ab84f
Alex Liu <liual@oocl.com>
2020-11-27 16:37:30 +0800

    [ParkingBoy][Alex Liu] refactor: changed variables to final

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,8 +4,8 @@ package com.oocl.cultivation;
 import java.util.HashMap;
 
 public class ParkingLot {
-    private int capacity;
-    private HashMap<Ticket, Car> ticketCarHashMap;
+    private final int capacity;
+    private final HashMap<Ticket, Car> ticketCarHashMap;
     public ParkingLot(int capacity) {
         this.capacity = capacity;
         this.ticketCarHashMap = new HashMap<>();
