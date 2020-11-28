c500e291b0e726cb6f1ad54d0826f10a5441206f
Alex <mingzhangliu@gmail.com>
2020-11-28 22:50:18 +0800

    [ParkingBoy Story 6][Alex Liu] feat: implemented test parking lot service manager add parking boys to management list

--- src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -1,12 +1,19 @@
 package com.oocl.cultivation;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingLotServiceManager {
+    private List<ParkingBoy> managementList;
+
+    public ParkingLotServiceManager() {
+        managementList = new ArrayList<>();
+    }
     public void addToManagementList(ParkingBoy parkingBoy) {
+        managementList.add(parkingBoy);
     }
 
-    public Collection<Object> getManagementList() {
-        return null;
+    public List<ParkingBoy> getManagementList() {
+        return this.managementList;
     }
 }
