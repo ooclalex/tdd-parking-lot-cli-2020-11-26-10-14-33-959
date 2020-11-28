5d6c939d3683bf308f78bd373a20ce78bdeeb8f5
Alex <mingzhangliu@gmail.com>
2020-11-28 15:35:55 +0800

    [ParkingBoy Story 5][Alex Liu] test: added case where should throw not enough position exception when super smart parking boy parks a car in a full parking lot given super smart parking boy, car and parking lots

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SuperSmartParkingBoyTest {
 
@@ -26,5 +27,29 @@ public class SuperSmartParkingBoyTest {
 
         //then
         assertEquals(car, parkingLot1.fetchCar(ticket));
+    }
+
+    @Test
+    void should_throw_not_enough_position_exception_when_park_the_car_given_super_smart_parking_boy_full_parking_lots() throws NotEnoughPositionException {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        smartParkingBoy.park(car1);
+        smartParkingBoy.park(car2);
+        smartParkingBoy.park(car3);
+
+        //when
+        NotEnoughPositionException notEnoughPosition =
+                assertThrows(NotEnoughPositionException.class,() -> smartParkingBoy.park(new Car()));
+
+        //then
+        assertEquals("Not Enough Position", notEnoughPosition.getMessage());
     }
 }
