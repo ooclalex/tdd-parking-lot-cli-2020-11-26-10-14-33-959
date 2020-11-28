605155d74c1a244b93ad3cb273a3f06495947bb1
Alex <mingzhangliu@gmail.com>
2020-11-28 15:31:28 +0800

    [ParkingBoy Story 5][Alex Liu] refactor: fix typo

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -10,10 +10,10 @@ public class SuperSmartParkingBoy extends ParkingBoy {
 
     @Override
     public Ticket park(Car car) throws NotEnoughPositionException {
-        ParkingLot largerAvailablePostionRateParkingLot = this.getParkingLots().stream().min(
+        ParkingLot largerAvailablePositionRateParkingLot = this.getParkingLots().stream().min(
                 Comparator.comparing(lot -> lot.getNumEmptyPositions() / lot.getCapacity())).orElse(null);
-        if (largerAvailablePostionRateParkingLot != null) {
-            return largerAvailablePostionRateParkingLot.park(car);
+        if (largerAvailablePositionRateParkingLot != null) {
+            return largerAvailablePositionRateParkingLot.park(car);
         }
         return null;
     }
